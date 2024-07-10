b5f38765fe0236f69c2a761d1ed9ca90488723b6
caero de deus <dedeusrh@gmail.com>
2024-07-10 16:39:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// toggle class active
+
+const navbarNav =document.querySelector('.navbar-nav');
+
+// se kuandu hamburger menu klik
+
+document.querySelector('#hamburger-menu').onclick = () => {
+
+navbarNav.classList.toggle('active');
+
+};
+// klik iha liur nav bar
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e){
+	if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+		navbarNav.classList.remove('active');
+	}
+})
